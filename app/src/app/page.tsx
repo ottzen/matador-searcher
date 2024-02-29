a0d2a9c3db0470b9f4ee36e8614e92e4486b52f4
Kasper Ottzen <ko@picopublish.com>
2024-02-29 13:18:54 +0100

    search component

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,7 +1,85 @@
+"use client"
 import Image from 'next/image'
 import styles from './page.module.css'
+import SubtitleSearch from './components/SubtitleSearch/SubtitleSearch.component'
 
 export default function Home() {
+  const srtContent = `
+  00:02:56,500 --> 00:02:59,333
+  Kvik, kom her.
+  
+  00:03:09,300 --> 00:03:13,009
+  - Dav, Boldt. Dav med jer.
+  - Dav, Larsen.
+
+  00:03:13,220 --> 00:03:17,577
+  Boldt, hvad står den på i dag?
+  
+  00:03:17,780 --> 00:03:23,537
+  Det ser sgu godt ud.
+  Så må jeg hellere få en dram til.
+  
+  00:03:23,740 --> 00:03:27,130
+  Der er '12.30' til Næstved.
+  
+  00:03:35,500 --> 00:03:38,776
+  Nå, Fede, skal du hjem og se,
+  hvad mor har til middag?
+  
+  00:03:39,060 --> 00:03:41,335
+  Der er din gode ven, Røde.
+  
+  00:03:41,540 --> 00:03:45,294
+  Bankdirektør Varnæs med konen.
+  De skal med toget.
+  
+  00:03:45,500 --> 00:03:51,370
+  De skal hente. Fruens søster
+  og børnene har været på ferie.
+  
+  00:03:51,580 --> 00:03:55,858
+  Boldt har kontakt
+  med de højere kredse.
+  
+  00:03:56,060 --> 00:03:59,370
+  Den kontakt kunne De vist
+  godt tænke Dem, Lauritz Jensen.
+  
+  00:03:59,580 --> 00:04:04,449
+  Røde interesserer sig sgu ikke
+  for kvinder, kun for politik.
+  
+  00:04:27,780 --> 00:04:32,092
+  - Her er vi.
+  - Hvem er det? Regitze!
+  
+  00:04:32,300 --> 00:04:39,172
+  - Er hun ikke for tyndt klædt på?
+  - Kom så, Ulrik! Han var her lige.
+  
+  00:04:39,380 --> 00:04:44,010
+  - Hvis han nu er faldet af toget?
+  - Vi skal bare finde ham.
+  
+  00:04:44,220 --> 00:04:47,292
+  Jamen så led da!
+  
+  00:05:00,540 --> 00:05:05,091
+  - Han er der ikke.
+  - Er det ham her?
+  
+  00:05:05,300 --> 00:05:09,771
+  - Nej, det ved gud det ikke er.
+  - Hvad bestiller De med min dreng?
+  
+  00:05:09,980 --> 00:05:12,448
+  Undskyld, jeg troede ...
+  
+  00:05:12,660 --> 00:05:20,055
+  - Jamen der har vi jo Ulrik.
+  - Jeg har låst mig inde!
+`;
+
   return (
     <main className={styles.main}>
       <div className={styles.description}>
@@ -27,6 +105,9 @@ export default function Home() {
           </a>
         </div>
       </div>
+
+      <h1>Subtitle Search App</h1>
+      <SubtitleSearch srtContent={srtContent} />
 
       <div className={styles.center}>
         <Image
